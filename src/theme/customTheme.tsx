ded92402edf35d1a3c9a725d5df2c5034a6d02ae
Navid <navidjalili3@gmail.com>
2023-07-18 12:53:08 +0330

    Move custom theme to its own directory

---
+++ src/theme/customTheme.tsx
@@ -0,0 +1,16 @@
+export const customTheme = extendTheme({
+    styles: {
+        global: {
+            "::-webkit-scrollbar": {
+                width: "8px",
+            },
+            "::-webkit-scrollbar-track": {
+                backgroundColor: "gray.100",
+            },
+            "::-webkit-scrollbar-thumb": {
+                backgroundColor: "gray.500",
+                borderRadius: "8px",
+            },
+        },
+    },
+});
