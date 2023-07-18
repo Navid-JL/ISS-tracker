e754eb07deda5cdd8c63752b052812abf33213b6
Navid <navidjalili3@gmail.com>
2023-07-18 12:49:42 +0330

    Update Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,20 @@
+import { Box, Container, Flex, Heading } from "@chakra-ui/react";
+import { GiBeamSatellite } from "react-icons/gi";
+
 const Header = () => {
-    return <header>Header</header>;
+    return (
+        <header>
+            <Box w="100%" bg="gray.500" padding="3" color="white">
+                <Container>
+                    <Flex alignItems="center" verticalAlign="center" justifyContent="center">
+                        <Heading as="h1" size="lg" mr="2">
+                            ISS Tracker
+                        </Heading>
+                        <GiBeamSatellite size="24px" />
+                    </Flex>
+                </Container>
+            </Box>
+        </header>
+    );
 };
 export default Header;
